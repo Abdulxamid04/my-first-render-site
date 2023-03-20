c4aa24c313d0bba90b123d309d41729229dae06d
Abdulxamid04 <sayfullayevabdulxamid@gmail.com>
2023-03-20 16:13:14 +0500

    intilaizing commit

--- app.js
+++ app.js
@@ -49,8 +49,10 @@ class RandomUser{
     }
 }
 
+
 document.getElementById('generate-btn').addEventListener('click', () => {
     RandomUser.fetchFromAPI();
 });
 
 RandomUser.fetchFromAPI();
+
---
+++ appp.js
@@ -0,0 +1,9 @@
+let comment = document.querySelector('card-body')
+let idcomment = document.querySelector('list')
+let email = document.querySelector('email')
+let body = document.querySelector('body')
+let btn = document.querySelector('btn btn-primary')
+let idcollaps = document.querySelector('collapseExample')
+
+
+
